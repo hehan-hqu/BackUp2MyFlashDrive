4816356e7aa8667dbea2224ee70b1d520a34e9cf
hehan-hqu <hehan201206@gmail.com>
2018-02-27 22:33:24 +0900

    Update 备份到U盘.bat

--- 备份到U盘.bat
+++ 备份到U盘.bat
@@ -4,7 +4,7 @@ set Save1=D:\C++\Projects\
 set Save2=F:\autotomcat\
 set Com=D:\C++\Projects\hone\
 set yymmdd=%DATE:~0,4%%DATE:~5,2%%DATE:~8,2%
-echo "备份文件到U盘autotomcat"
+echo "澶囦唤鏂囦欢鍒癠鐩榓utotomcat"
 "%Sz%" a "%Save1%%yymmdd%hone.7z" "%Com%"
 copy "%Save1%%yymmdd%hone.7z" F:\autotomcat\
 pause
